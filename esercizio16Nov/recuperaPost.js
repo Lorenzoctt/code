526bff47c9a7dbab4e06c261113b4075bed93ad1
Lorenzoctt <lorenzo.ciatti64@gmail.com>
2020-11-19 18:59:58 +0100

    kk

--- esercizio16Nov/recuperaPost.js
+++ esercizio16Nov/recuperaPost.js
@@ -1,19 +1,27 @@
+// ok
+
 const myUrl = "https://jsonplaceholder.typicode.com/users";
 
 
 
-const myButton = document.getElementById("keyfetch");
 
+const myButton = document.getElementById("keyfetch");
+myButton.addEventListener("click", fetchData);
+
+
+function fetchshort() {
+    fetch(myUrl)
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
 
 //recupera un oggetto da internet e aggiunge la lista li sullo schermo 
 function fetchData() {
     console.log("inizio");
 
-    // fetch('http://example.com/movies.json')
-    //     .then(response => response.json())
-    //     .then(data => console.log(data));
 
-    fetch(myUrl)
+
+    fetch(myUrl)  // fetch è asicnrono
         .then((response) => {
             if (!response.ok) throw Error(response.statusText); //throw genera l'errore 
             return response.json();//se ok si estrae la risposta nel formato json 
@@ -43,4 +51,3 @@ function fetchData() {
 
 
 
-myButton.addEventListener("click", fetchData);
